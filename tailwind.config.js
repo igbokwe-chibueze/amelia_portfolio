89db937a570090a6aaa1395c4d1fa6406b44507e
igbokwe-chibueze <igbokwechibueze@gmail.com>
2024-01-02 14:31:40 +0100

    Created folder structure and primary files.
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        baseFont: ['DM Sans', 'sans-serif'],
+      },
+      colors: {
+        'primary-color': '#edf2f8',
+        'secondary-color': '#313bac',
+        'black-color': '#030303',
+        'lightGray-color': '#e4e4e4',
+        'gray-color': '#6b7688',
+        'brown-color': '#46364a',
+        'white-color': '#ffffff'
+      },
+    },
   },
   plugins: [],
 }
