7d9801490c6c35eed87896962bc19beb685ef071
igbokwe-chibueze <igbokwechibueze@gmail.com>
2024-04-10 04:24:52 +0100

    Header now using the FetchData Custom Hook
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { CustomBtn, ParallaxText } from '../components';
+import { useState } from 'react';
+import { CustomBtn, FetchData, ParallaxText } from '../components';
 import { ArrowDownIcon } from '../constants/icons';
 import { motion } from 'framer-motion';
 import Typewriter from 'typewriter-effect';
@@ -67,28 +67,16 @@ const hoverScaleVariants = {
   },
 };
 
+const iconIndex = {
+  0 : 'w-16 h-16 tablet:w-24 tablet:h-24 -mt-9 tablet:-mt-0 tablet:-ml-14',
+  1 : 'w-20 h-20 tablet:w-28 tablet:h-28 tablet:ml-10',
+  2 : 'w-16 h-16 tablet:w-20 tablet:h-20 -mt-10 tablet:-mt-0 tablet:-ml-8',
+  3 : 'w-16 h-16 tablet:w-20 tablet:h-20 -mt-10 tablet:-mt-0 tablet:-ml-8',
+};
+
 const Header = () => {
 
-  const [header, setHeader] = useState([]);
-
-  // Fetch header data using the 'client' object on component mount
-  useEffect(() => {
-    // Define queries to fetch data from the 'header' from Sanity
-    const query = '*[_type == "header"][0]'; // Fetch the first header object.
-
-    // Fetch header data
-    client.fetch(query).then((data) => {
-      setHeader(data);
-    });
-
-  }, []);
-
-  const iconIndex = {
-    0 : 'w-16 h-16 tablet:w-24 tablet:h-24 -mt-9 tablet:-mt-0 tablet:-ml-14',
-    1 : 'w-20 h-20 tablet:w-28 tablet:h-28 tablet:ml-10',
-    2 : 'w-16 h-16 tablet:w-20 tablet:h-20 -mt-10 tablet:-mt-0 tablet:-ml-8',
-    3 : 'w-16 h-16 tablet:w-20 tablet:h-20 -mt-10 tablet:-mt-0 tablet:-ml-8',
-  };
+  const { error, isPending, data: header } = FetchData('*[_type == "header"][0]')
 
   const [downloading, setDownloading] = useState(false);
 
@@ -135,145 +123,149 @@ const Header = () => {
 
   return (
     <section id='header' className="w-full min-h-screen pt-16 px-2">
-      <div className="bg-midnight-green rounded-lg mb-8">
-        <div className="bg-caribbean-current text-tea-green rounded-t-lg p-3 tablet:p-4">
-          <ParallaxText baseVelocity={2} clamp={true}>{header.parallaxText}</ParallaxText>
-        </div>
+      {error && <div>{error}</div>}
+      {isPending && (<p>Loading......</p>)}
+      {!isPending && header && (
+        <div className="bg-midnight-green rounded-lg mb-8">
+          <div className="bg-caribbean-current text-tea-green rounded-t-lg p-3 tablet:p-4">
+            <ParallaxText baseVelocity={2} clamp={true}>{header.parallaxText}</ParallaxText>
+          </div>
 
-        <div className="grid grid-cols-1 tablet:grid-cols-3 gap-4 mt-4 mx-4">
-          {/* First Column */}
-          <div className="tablet:col-span-1">
-            <motion.div
-              variants={textSlideVariants}
-              whileInView={textSlideVariants.whileInView}
-              className="flex flex-col gap-5 text-tea-green"
-            >
-              <h4 className="flex space-x-2 text-lg font-normal">
-                <span>👋🏿</span>
-                <Typewriter
-                  options={{
-                    strings: header.salutations,
-                    autoStart: true,
-                    loop: true,
-                    delay: 70, // Time to complete each string
-                    pauseFor: 6000, // Optional pause after each string
-                    escapeHtml: false, // Allow HTML in strings
-                    cursor: '..',
-                  }}
-                />
-              </h4>
-              
+          <div className="grid grid-cols-1 tablet:grid-cols-3 gap-4 mt-4 mx-4">
+            {/* First Column */}
+            <div className="tablet:col-span-1">
               <motion.div
-                variants={textChildrenVarients}
-                whileInView={window.innerWidth > 639 ? textChildrenVarients.whileInView : textChildrenVarients.whileInViewMobile }
-                transition={{ delay:1, duration: 1 }}
-                className="flex flex-col gap-5"
+                variants={textSlideVariants}
+                whileInView={textSlideVariants.whileInView}
+                className="flex flex-col gap-5 text-tea-green"
               >
-                <h1 className="text-6xl font-bold">
-                  Hi, I&apos;m <span className="text-chartreuse-color capitalize">Amelia Olufowobi</span>
-                </h1>
-              </motion.div>
-
-              <motion.div
-                variants={textChildrenVarients}
-                whileInView={window.innerWidth > 639 ? textChildrenVarients.whileInView : textChildrenVarients.whileInViewMobile }
-                transition={{ delay:1, duration: 1.4 }} 
-                className='flex flex-col gap-5'
-              >
-                <h2 className="flex space-x-2 text-2xl tablet:text-4xl font-bold">
-                  <span>a</span>
+                <h4 className="flex space-x-2 text-lg font-normal">
+                  <span>👋🏿</span>
                   <Typewriter
                     options={{
-                      strings: header.professions,
+                      strings: header.salutations,
                       autoStart: true,
                       loop: true,
-                      delay: 50, // Time to complete each string
-                      pauseFor: 1000, // Optional pause after each string
+                      delay: 70, // Time to complete each string
+                      pauseFor: 6000, // Optional pause after each string
                       escapeHtml: false, // Allow HTML in strings
+                      cursor: '..',
                     }}
                   />
-                </h2>
-                <p className="text-base leading-6 tracking-wide">
-                  {header.remark}
-                </p>
-              </motion.div>
-            </motion.div>
-          </div>
-
-          {/* Second Column */}
-          <div className="tablet:col-span-1">
-            <motion.div
-              whileInView={{ y: [100, 0], opacity: [0, 1] }} // Animation effect for horizontal sliding from left and opacity change from transparent to full
-              transition={{ duration: 1.5 }}
-              className="flex justify-center items-center"
-            >
-              {header.imgurl ? (
-                <img 
-                  src={urlFor(header.imgurl)} 
-                  alt="profile_bg" 
-                  className="object-contain z-10 tablet:-mt-14 p-0 w-[350px] h-[350px] tablet:w-[450px] tablet:h-[450px]"
-                />
-              ) : (
-                "" // In the absence of a profile picture, leave the area blank. I can also place a temporal picture here.
-              )}
-            </motion.div>
-          </div>
-
-          {/* Third Column */}
-          <div className="tablet:col-span-1">
-            <motion.div 
-              variants={scaleVariants} // Animation variants for scale effect
-              whileInView={scaleVariants.whileInView}
-              className='flex tablet:flex-col justify-between items-center 
-              tablet:items-start tablet:space-y-12 pb-4 tablet:pb-0 -mt-2 tablet:-mt-0'
-            >
-              {header.bestTools && header.bestTools.slice(0, 3).map((bestTool, index) => (
+                </h4>
+                
                 <motion.div
-                    key={index}
-                    variants={hoverScaleVariants}
-                    whileHover={hoverScaleVariants.hover}
-                    className={`hearder-icon-circles header${iconIndex[index]}`}
+                  variants={textChildrenVarients}
+                  whileInView={window.innerWidth > 639 ? textChildrenVarients.whileInView : textChildrenVarients.whileInViewMobile }
+                  transition={{ delay:1, duration: 1 }}
+                  className="flex flex-col gap-5"
                 >
-                    <img
-                        key={index}
-                        src={urlFor(bestTool).url()}
-                        alt={`Image ${index}`}
-                        className="object-cover rounded-2xl w-full h-full"
-                    />
+                  <h1 className="text-6xl font-bold">
+                    Hi, I&apos;m <span className="text-chartreuse-color capitalize">Amelia Olufowobi</span>
+                  </h1>
                 </motion.div>
-              ))}
+
+                <motion.div
+                  variants={textChildrenVarients}
+                  whileInView={window.innerWidth > 639 ? textChildrenVarients.whileInView : textChildrenVarients.whileInViewMobile }
+                  transition={{ delay:1, duration: 1.4 }} 
+                  className='flex flex-col gap-5'
+                >
+                  <h2 className="flex space-x-2 text-2xl tablet:text-4xl font-bold">
+                    <span>a</span>
+                    <Typewriter
+                      options={{
+                        strings: header.professions,
+                        autoStart: true,
+                        loop: true,
+                        delay: 50, // Time to complete each string
+                        pauseFor: 1000, // Optional pause after each string
+                        escapeHtml: false, // Allow HTML in strings
+                      }}
+                    />
+                  </h2>
+                  <p className="text-base leading-6 tracking-wide">
+                    {header.remark}
+                  </p>
+                </motion.div>
+              </motion.div>
+            </div>
+
+            {/* Second Column */}
+            <div className="tablet:col-span-1">
+              <motion.div
+                whileInView={{ y: [100, 0], opacity: [0, 1] }} // Animation effect for horizontal sliding from left and opacity change from transparent to full
+                transition={{ duration: 1.5 }}
+                className="flex justify-center items-center"
+              >
+                {header.imgurl ? (
+                  <img 
+                    src={urlFor(header.imgurl)} 
+                    alt="profile_bg" 
+                    className="object-contain z-10 tablet:-mt-14 p-0 w-[350px] h-[350px] tablet:w-[450px] tablet:h-[450px]"
+                  />
+                ) : (
+                  "" // In the absence of a profile picture, leave the area blank. I can also place a temporal picture here.
+                )}
+              </motion.div>
+            </div>
+
+            {/* Third Column */}
+            <div className="tablet:col-span-1">
+              <motion.div 
+                variants={scaleVariants} // Animation variants for scale effect
+                whileInView={scaleVariants.whileInView}
+                className='flex tablet:flex-col justify-between items-center 
+                tablet:items-start tablet:space-y-12 pb-4 tablet:pb-0 -mt-2 tablet:-mt-0'
+              >
+                {header.bestTools && header.bestTools.slice(0, 3).map((bestTool, index) => (
+                  <motion.div
+                      key={index}
+                      variants={hoverScaleVariants}
+                      whileHover={hoverScaleVariants.hover}
+                      className={`hearder-icon-circles header${iconIndex[index]}`}
+                  >
+                      <img
+                          key={index}
+                          src={urlFor(bestTool).url()}
+                          alt={`Image ${index}`}
+                          className="object-cover rounded-2xl w-full h-full"
+                      />
+                  </motion.div>
+                ))}
+
+              </motion.div>
+            </div>
+          </div>
+
+          <div className="flex justify-center items-center mt-4">
+            <motion.div
+              variants={buttonVariants}
+              // Apply hover effect only when not downloading. I used rest when downloading or btn wont return to no rotation.
+              whileHover={!downloading ? buttonVariants.hover : 'rest'}
+              initial="rest" // Optional: Set the initial state
+              className='-mt-4 tablet:-mt-14 z-20'
+            >
+              <CustomBtn
+                classProps={`${downloading ? 'rotate-12' : ''} px-8 py-2`}
+                label={downloading ? 'Downloading...' : 'Download CV'}
+                btnType="button"
+                onBtnClick={handleDownloadClick}
+                disabled={downloading}  // Disable the button during download
+              >
+                <ArrowDownIcon 
+                  className={`w-[50px] h-[50px] text-chartreuse-color fill-midnight-green 
+                  ${!downloading ? '-rotate-45' : 'rotate-90 text-[#81926D] fill-midnight-green animate-bounce'}`}
+                />
+              </CustomBtn>
 
             </motion.div>
           </div>
         </div>
+      )}
 
-        <div className="flex justify-center items-center mt-4">
-          <motion.div
-            variants={buttonVariants}
-            // Apply hover effect only when not downloading. I used rest when downloading or btn wont return to no rotation.
-            whileHover={!downloading ? buttonVariants.hover : 'rest'}
-            initial="rest" // Optional: Set the initial state
-            className='-mt-4 tablet:-mt-14 z-20'
-          >
-            <CustomBtn
-              classProps={`${downloading ? 'rotate-12' : ''} px-8 py-2`}
-              label={downloading ? 'Downloading...' : 'Download CV'}
-              btnType="button"
-              onBtnClick={handleDownloadClick}
-              disabled={downloading}  // Disable the button during download
-            >
-              <ArrowDownIcon 
-                className={`w-[50px] h-[50px] text-chartreuse-color fill-midnight-green 
-                ${!downloading ? '-rotate-45' : 'rotate-90 text-[#81926D] fill-midnight-green animate-bounce'}`}
-              />
-            </CustomBtn>
-
-          </motion.div>
-        </div>
-      </div>
     </section>
   );
 };
 
-//export default AppWrap(Header, 'header');
 export default Header;
