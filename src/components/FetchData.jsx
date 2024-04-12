f8f19e12dd27b73bdf56de215483e5bd2e374e3d
igbokwe-chibueze <igbokwechibueze@gmail.com>
2024-04-12 06:29:23 +0100

    Removed Delay in HeaderTwo Branch
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -16,11 +16,8 @@ const FetchData = (query) => {
   
       // Set pending state to true when the query changes
       //setIsPending(true);
-  
-      // Simulate a delay using setTimeout
-      const timeout = setTimeout(() => {
-        // Fetch data using the 'client' and update the 'data' state with the fetched data
-        client.fetch(query)
+
+      client.fetch(query)
           .then((fetchedData) => {
             // Data fetched successfully, update state
             setData(fetchedData);
@@ -35,10 +32,30 @@ const FetchData = (query) => {
             // Set pending state to false
             setIsPending(false);
           });
-      }, 1000); // Simulate a delay of 1 second
   
-      // Clean up function to clear the timeout if component unmounts or query changes
-      return () => clearTimeout(timeout);
+      // // Simulate a delay using setTimeout
+      // const timeout = setTimeout(() => {
+      //   // Fetch data using the 'client' and update the 'data' state with the fetched data
+      //   client.fetch(query)
+      //     .then((fetchedData) => {
+      //       // Data fetched successfully, update state
+      //       setData(fetchedData);
+      //       // Set pending state to false
+      //       setIsPending(false);
+      //     })
+      //     .catch((error) => {
+      //       // Handle error
+      //       console.error('Error fetching data:', error);
+      //       // Update error state
+      //       setError(error);
+      //       // Set pending state to false
+      //       setIsPending(false);
+      //     });
+      // }, 1000); // Simulate a delay of 1 second
+  
+      // // Clean up function to clear the timeout if component unmounts or query changes
+      // return () => clearTimeout(timeout);
+
     }, [query]);
   
     // Return the fetched data, error, and pending state
